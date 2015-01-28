378eaa39fd0491953ba7a5662756732368c1a3df
Justin Lai <jwlai@ucsd.edu>
2015-01-28 00:03:15 -0800

    Step 4 - Done

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,23 +2,25 @@
 
 /* jasmine specs for controllers go here */
 
-describe('PhoneListCtrl', function(){
+describe('PhoneCat controllers', function() {
 
-  beforeEach(module('phonecatApp'));
+  describe('PhoneListCtrl', function(){
+    var scope, ctrl;
 
-  it('should create "phones" model with 3 phones', inject(function($controller) {
-    var scope = {},
-        ctrl = $controller('PhoneListCtrl', {$scope:scope});
+    beforeEach(module('phonecatApp'));
 
-    expect(scope.phones.length).toBe(4);
-    expect(scope.name).toBe('World');
-  }));
+    beforeEach(inject(function($controller) {
+      scope = {};
+      ctrl = $controller('PhoneListCtrl', {$scope:scope});
+    }));
 
-  it('should display the current filter value in the title bar', function() {
-      query.clear();
-      expect(browser.getTitle()).toMatch(/Google Phone Gallery:\s*$/);
-
-      query.sendKeys('nexus');
-      expect(browser.getTitle()).toMatch(/Google Phone Gallery: nexus$/);
+    it('should create "phones" model with 3 phones', function() {
+      expect(scope.phones.length).toBe(3);
     });
+
+
+    it('should set the default value of orderProp model', function() {
+      expect(scope.orderProp).toBe('age');
+    });
+  });
 });
