c1233972aef3d2aa86bef9dd7411e7eca69718a4
Justin Lai <jwlai@ucsd.edu>
2015-01-29 16:38:04 -0800

    Step 7 Done

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,14 +1,19 @@
 'use strict';
 
 /* Controllers */
-var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
-  function ($scope, $http) {
+var phonecatControllers = angular.module('phonecatControllers', []);
+
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
+  function($scope, $http) {
     $http.get('phones/phones.json').success(function(data) {
-      $scope.phones = data.splice(0, 5);
+      $scope.phones = data;
     });
 
-
     $scope.orderProp = 'age';
+  }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    $scope.phoneId = $routeParams.phoneId;
   }]);
